6ec529bdd7d7b7c2538630d550e96bae5529ad0c
Joe Davault <joe.davault@gmail.com>
2021-10-06 23:04:31 -0700

    Setting up Redux

---
+++ redux/configureStore.js
@@ -0,0 +1,22 @@
+add a file named configureStore.js in the redux folder and add the following to it:
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { campsites } from './campsites';
+import { comments } from './comments';
+import { promotions } from './promotions';
+import { partners } from './partners';
+
+export const ConfigureStore = () => {
+  const store = createStore(
+    combineReducers({
+      campsites,
+      comments,
+      partners,
+      promotions
+    }),
+    applyMiddleware(thunk, logger)
+  );
+
+  return store;
+}
